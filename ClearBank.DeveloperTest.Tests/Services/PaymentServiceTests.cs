e2412b25414597d8d052789a52390329185200ba
Alan Gardner <bigalan09@gmail.com>
2024-11-07 12:48:02 +0000

    Setup test project

---
+++ ClearBank.DeveloperTest.Tests/Services/PaymentServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using AutoFixture;
+using AutoFixture.Xunit2;
+using ClearBank.DeveloperTest.Services;
+using FluentAssertions;
+using Xunit;
+
+namespace ClearBank.DeveloperTest.Tests.Services
+{
+    public class PaymentServiceTests
+    {
+        [Theory]
+        [AutoData]
+        public void MakePayment_WithValidRequst_ReturnsSuccessful(
+            PaymentService sut,
+            IFixture fixture)
+        {
+            // Arrange
+            Types.MakePaymentRequest request = fixture.Create<Types.MakePaymentRequest>();
+
+            // Act
+            var result = sut.MakePayment(request: request);
+            
+            // Assert
+            result.Should().NotBeNull();
+            result.Success.Should().BeTrue();
+        }
+    }
+}
