40a878dd064cffe777787cae890cc42d499d4ee0
Alan Gardner <bigalan09@gmail.com>
2024-11-07 13:30:19 +0000

    Added options for the DataFactory tests

---
+++ ClearBank.DeveloperTest/Options/DataStoreOptions.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClearBank.DeveloperTest.Options
+{
+    public class DataStoreOptions
+    {
+        [Required]
+        public required string Type { get; init; } = "Default";
+    }
+}
