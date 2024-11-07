623e77479bc0e3229cb455e4cefe38a7ff0b95ee
Alan Gardner <bigalan09@gmail.com>
2024-11-07 13:30:04 +0000

    Changed AccountDataStores to allow nullable Account when using GetAccount.

---
+++ ClearBank.DeveloperTest/Data/AccountDataStoreFactory.cs
@@ -0,0 +1,29 @@
+﻿using ClearBank.DeveloperTest.Options;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace ClearBank.DeveloperTest.Data
+{
+    internal class AccountDataStoreFactory : IAccountDataStoreFactory
+    {
+        private const string BackupTypeName = "Backup";
+        private readonly IOptions<DataStoreOptions> _options;
+        private readonly IServiceProvider _provider;
+
+        public AccountDataStoreFactory(
+            IOptions<DataStoreOptions> options,
+            IServiceProvider provider)
+        {
+            _options = options;
+            _provider = provider;
+        }
+
+        public IAccountDataStore Create()
+        {
+            return _options.Value.Type == BackupTypeName
+                ? _provider.GetRequiredService<BackupAccountDataStore>()
+                : _provider.GetRequiredService<AccountDataStore>();
+        }
+    }
+}
--- ClearBank.DeveloperTest/Data/IAccountDataStore.cs
+++ ClearBank.DeveloperTest/Data/IAccountDataStore.cs
@@ -4,7 +4,7 @@ namespace ClearBank.DeveloperTest.Data
 {
     public interface IAccountDataStore
     {
-        Account GetAccount(string accountNumber);
+        Account? GetAccount(string accountNumber);
         void UpdateAccount(Account account);
     }
 }
